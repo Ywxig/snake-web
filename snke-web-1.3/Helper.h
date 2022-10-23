b65697440d1ef9fb529ac2775310d743e390c1f4
Ywxig <82500877+Ywxig@users.noreply.github.com>
2022-10-23 15:23:09 +0300

    Add files via upload

---
+++ snke-web-1.3/Helper.h
@@ -0,0 +1,23 @@
+// Начнем с директив препроцессора. ADD_H – это произвольное уникальное имя (обычно используется имя заголовочного файла)
+#ifndef HELPER_H
+#define HELPER_H
+
+#include <vector>
+#include <string>
+using namespace std;
+#pragma once
+
+// в этом файле описаны прототипы функций для работы нашего инторпритатора
+
+vector<string> split(string str, string sep = " "); // прототип функции split() (не забывайте точку с запятой в конце!)
+
+void WriteInFile(string file_for_input, string string_for_input); // прототип функции добавления в фаил строки
+string Read(string file_to_read);// функция чьтения файла
+vector<string> SplitOnFild(string content); // Прототип функции для разбивания на поля исходный текст
+void AddInFile(string file_for_input, string string_for_input); // Функция перезаписи файла
+
+void log(string log_text);// инструмент записи логов
+
+string int_to_str(int num);// функция для перевода иш int в str
+
+#endif
